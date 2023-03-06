6ad2589c40d389be3011ce15aef2c1aeafadda27
Nancy Hartman <103781121+nancyah@users.noreply.github.com>
2023-03-06 10:35:10 -0600

    Update Python Functions Medical Insurance Project.py
    optimize code

--- Python Functions Medical Insurance Project.py
+++ Python Functions Medical Insurance Project.py
@@ -4,27 +4,9 @@ def calculate_insurance_cost(age, sex, bmi, num_of_children, smoker):
   print('The estimated insurance cost for this person is ' + str(estimated_cost) + ' dollars.')
   return estimated_cost
 
-# Initial variables for Maria 
-age = 28
-sex = 0  
-bmi = 26.2
-num_of_children = 3
-smoker = 0  
-
 # Estimate Maria's insurance cost
-insurance_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
-
-print("The estimated insurance cost for Maria is " + str(insurance_cost) + " dollars.")
-
-# Initial variables for Omar
-age = 35
-sex = 1 
-bmi = 22.2
-num_of_children = 0
-smoker = 1  
+maria_insurance_cost = calculate_insurance_cost(28, 0, 26.2, 3, 0)  
 
 # Estimate Omar's insurance cost 
-insurance_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
-
-print("The estimated insurance cost for Omar is " + str(insurance_cost) + " dollars.")
+omar_insurance_cost = calculate_insurance_cost(35, 1, 22.2, 0, 1)
 
